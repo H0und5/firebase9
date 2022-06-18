953e35573a77c5411e117ccb0a779366077f27b0
H0und5 <84218198+H0und5@users.noreply.github.com>
2022-06-18 18:37:44 -0500

    feature: learning about firestore, initializeApp, getFirestore to connect to db, collection to reference, and getDocs to get the documents in a collection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log('yo was good');
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore, collection, getDocs
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0QeL9lAeRtzFgHWrkxkukykI7q7NVvx0",
+  authDomain: "fir-9-41660.firebaseapp.com",
+  projectId: "fir-9-41660",
+  storageBucket: "fir-9-41660.appspot.com",
+  messagingSenderId: "576339317768",
+  appId: "1:576339317768:web:2aa6d17a60c406a996adbd"
+};
+
+
+// init firebase app
+initializeApp(firebaseConfig);
+
+
+// init services
+const db = getFirestore();
+
+// collections ref
+const colRef = collection(db, 'books');
+
+// get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    let books = [];
+
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(books)
+  })
+  .catch(err => {
+    console.log(err.message)
+  });
